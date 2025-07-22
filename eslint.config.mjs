fbda40aad18a1f31656225cbea19fae0a9a73603
Yoanna Popova <yoanna.dp@gmail.com>
2025-07-22 08:42:54 +0300

    Initial commit for master branch.

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*test.js"], languageOptions: { globals: globals.jest } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+]);
